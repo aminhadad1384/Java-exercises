aae9ed97795d59a442d96d46afd2fc29a2ddc48f
aminhadad1384 <maminhadad84@gmail.com>
2025-10-03 10:28:23 +0330

    get len of array and complete array then show sum

---
+++ sum array elements/sum_arr_elements.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class sum_arr_elements {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter length of the array : ");
+        int arr_length = input.nextInt();
+        int sum = 0;
+        int[] numbers = new int[arr_length];
+        for (int i = 0; i < arr_length; i++) {
+            try {
+                numbers[i] = input.nextInt();
+            } catch (Exception e) {
+                System.out.println("error : " + e);
+            }
+            sum += numbers[i];
+        }
+        System.out.println("sumation of elements of array : " + sum);
+    }
+}
