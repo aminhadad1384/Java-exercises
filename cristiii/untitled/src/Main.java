5e1eac703701cacc779a2fd5e7219141272a0a37
aminhadad1384 <maminhadad84@gmail.com>
2025-11-22 19:21:54 +0330

    olaviat

--- cristiii/untitled/src/Main.java
+++ cristiii/untitled/src/Main.java
@@ -13,44 +13,21 @@ public class Main {
             private Node<E> _next;
             private Node<E> _prev;
 
-            public Node(E data) {
-                this._data = data;
+            public Node(E data) { this._data = data; }
+            public Node(E data, Node<E> prev, Node<E> next) {
+                this._data = data; this._next = next; this._prev = prev;
             }
-
-            public Node(E data, Node prev, Node next) {
-                this._data = data;
-                this._next = next;
-                this._prev = prev;
-            }
-
-            public Node<E> getNext() {
-                return _next;
-            }
-
-            public void setNext(Node<E> next) {
-                this._next = next;
-            }
-
-            public E getData() {
-                return _data;
-            }
-
-            public void setData(E data) {
-                this._data = data;
-            }
-
-            public Node<E> getPrev() {
-                return _prev;
-            }
-
-            public void setPrev(Node prev) {
-                this._prev = prev;
-            }
+            public Node<E> getNext() { return _next; }
+            public void setNext(Node<E> next) { this._next = next; }
+            public E getData() { return _data; }
+            public void setData(E data) { this._data = data; }
+            public Node<E> getPrev() { return _prev; }
+            public void setPrev(Node<E> prev) { this._prev = prev; }
         }
 
         public DoublyLinkedList() {
-            head = new Node(null,null,null);
-            tail = new Node(null,head,null);
+            head = new Node<>(null,null,null);
+            tail = new Node<>(null,head,null);
             head.setNext(tail);
         }
 
@@ -62,35 +39,8 @@ public class Main {
         }
 
         public boolean add(E e) {
-            addBetween(e, tail.getPrev(),tail);
+            addBetween(e, tail.getPrev(), tail);
             return true;
-        }
-
-        public void add(int index, E element) {
-            if(index<0 || index > size) throw new IndexOutOfBoundsException();
-            Node c = head;
-            for (int i = 0; i < index; i++)
-                c = c.getNext();
-            addBetween(element,c,c.getNext());
-        }
-
-        public void addFirst(E e) {
-            addBetween(e, head, head.getNext());
-        }
-
-
-        public E get(int index) {
-            if (index < 0 || index >= size)
-                throw new IndexOutOfBoundsException();
-            Node<E> c = head.getNext();
-            for (int i = 0; i < index; i++)
-                c = c.getNext();
-            return c.getData();
-        }
-
-        public E getFirst()  {
-            if(isEmpty()) throw new NoSuchElementException();
-            return head.getNext().getData();
         }
 
         public E getLast(){
@@ -106,84 +56,56 @@ public class Main {
             size--;
             return node.getData();
         }
-        public E remove(int index) throws IndexOutOfBoundsException {
-            if (index < 0 || index >= size)
-                throw new IndexOutOfBoundsException();
-            Node<E> c = head.getNext();
-            for (int i = 0; i < index; i++)
-                c = c.getNext();
-            return remove(c);
-        }
 
-        public boolean remove(E e) {
-            Node<E> c = head.getNext();
-            while(c!=tail) {
-                if (c.getData().equals(e)) {
-                    remove(c);
-                    return true;
-                }
-                c= c.getNext();
-            }
-            return false;
-        }
-
-        public E removeFirst() throws NoSuchElementException {
-            if(isEmpty()) throw new NoSuchElementException();
-            return remove(head.getNext());
-        }
-
-        public E removeLast() throws NoSuchElementException {
+        public E removeLast() {
             if(isEmpty()) throw new NoSuchElementException();
             return remove(tail.getPrev());
         }
 
+        public int size() { return size; }
+        public boolean isEmpty() { return size==0; }
 
-        public E set(int index, E e) throws IndexOutOfBoundsException {
+        public E get(int index) {
             if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
             Node<E> c = head.getNext();
-            for (int i = 0; i < index; i++) {
-                c = c.getNext();
-            }
-            E data = c.getData();
-            c.setData(e);
-            return data;
-        }
-
-
-        public int size() {
-            return size;
-        }
-
-        public boolean isEmpty() {
-            return size==0;
+            for (int i = 0; i < index; i++) c = c.getNext();
+            return c.getData();
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        for(int indexForT = 0;indexForT < t;indexForT ++){
-            DoublyLinkedList<Integer> d=new DoublyLinkedList<>();
-            int n=sc.nextInt();
-            int k=sc.nextInt();
-            //adding persons
-            for(int iindexForN=0;iindexForN<n;iindexForN++){
-                int data=sc.nextInt();
-                d.add(data);
+        for(int indexForTests=0; indexForTests<t; indexForTests++){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            DoublyLinkedList<Integer> d = new DoublyLinkedList<>();
+            //adding numbers
+            for(int AddIndex=0; AddIndex<n; AddIndex++){
+                d.add(sc.nextInt());
             }
             //removing
-            for(int i=0;i<k ;i++){
+            for(int DeleteIndex=0; DeleteIndex<k; DeleteIndex++){
+                boolean removed = false;
+                DoublyLinkedList.Node<Integer> node = d.head.getNext();
 
-                DoublyLinkedList.Node<Integer> node=d.head.getNext();
-                while(node != d.tail && node.getNext() != d.tail) {
-                    if((int)node.getData() <(int)node.getNext().getData()){
+                while(node.getNext() != d.tail){
+                    if(node.getData() < node.getNext().getData()){
                         d.remove(node);
-                        continue;
+                        removed = true;
+                        break;
                     }
-                    node=node.getNext();
+                    node = node.getNext();
                 }
-                d.remove((int)node.getData());
+                if(!removed && !d.isEmpty()){
+                    d.removeLast();
+                }
             }
-            System.out.println(d.size());
+
+            for(int i=0; i<d.size(); i++){
+                System.out.print(d.get(i) + " ");
+            }
+            System.out.println();
         }
     }
 }
